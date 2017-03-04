770e18504a5942766f5058aa5254870072f2a1f0
Spencer White <swhite215@live.com>
2017-03-04 11:48:15 -0500

    Altered git.js file

--- js/git.js
+++ js/git.js
@@ -14,23 +14,23 @@ $(document).ready(function() {
       $("div#profile").append(gitLog);
 
       //Git User avatar and save into image
-      var gitAvatar = $("<img>").attr("src", gitUser[i].avatar_url).attr("height", "300px").attr("width", "300px");
-      $(gitAvatar).addClass("gitImageAdjust");
+      var gitAvatar = $("<img>").attr("src", gitUser[i].avatar_url).attr("height", "300px").attr("width", "300px"); //Create Element to store img
+      $(gitAvatar).addClass("gitImageAdjust"); //add styling class
 
       //Link to Git Site
-      var gitLink = $("<a>").attr("href", gitUser[i].html_url).attr("target", "_blank");
-      gitLink.append(gitAvatar);
-      $("div#profile").append(gitLink);
+      var gitLink = $("<a>").attr("href", gitUser[i].html_url).attr("target", "_blank"); //Create anchor to store link to site
+      gitLink.append(gitAvatar); //Append Git img to anchor element
+      $("div#profile").append(gitLink); //Append anchor element and img to profile container
 
       //Number of Git Followers
-      var gitFollowers = $("<span>").text("Followers: " + gitUser[i].followers_url.length);
-      gitFollowers.addClass("spanStyle");
-      $("div#profile").append(gitFollowers);
+      var gitFollowers = $("<span>").text("Followers: " + gitUser[i].followers_url.length); //Create element to store followers data
+      gitFollowers.addClass("spanStyle"); //add styling class
+      $("div#profile").append(gitFollowers); //Append element to profile container
 
       //Number of Git Repos
-      var gitRepo = $("<span>").text("Repositories: " + gitUser[i].repos_url.length);
-      gitRepo.addClass("spanStyle repoSpace");
-      $("div#profile").append(gitRepo);
+      var gitRepo = $("<span>").text("Repositories: " + gitUser[i].repos_url.length); //Create element to store repo data
+      gitRepo.addClass("spanStyle repoSpace"); //add styling class
+      $("div#profile").append(gitRepo); //Append element to profile container
 
       //Add a line to space users apart.
       $("div#profile").append("<hr>");
